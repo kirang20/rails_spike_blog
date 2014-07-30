78178dcb8ec3ac87452421312c87d58671a7c443
Kiran G <kirang20@gmail.com>
2014-07-30 12:26:03 +0530

    Kiran | Added update article support.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,15 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new (article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
   def index
     @articles = Article.all
   end
@@ -11,12 +20,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def create
-    @article = Article.new (article_params)
-    if @article.save
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if (@article.update(article_params))
       redirect_to @article
     else
-      render 'new'
+      render 'edit'
     end
   end
 
